1ca31b984e798c6ecca52a328bc7f5d5bc1bedda
jean-francois dive <jdive@cisco.com>
2017-08-21 15:13:53 +0000

    meh

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,7 @@ docker run \
     -e DOORMAN_ENROLL_SECRET=foo \
     -e DOORMAN_SECRET_KEY=secret-key \
     -e POSTGRES_USER=postgres \
-    -e POSTGRES_PASSWORD=548b4a24751881ce735f89a27db4ba7087a3afbd \
+    -e POSTGRES_PASSWORD=doorman123 \
     -e POSTGRES_PORT=172.17.0.2:5432 \
     -e POSTGRES_DATABASE=doorman \
     -p 5000:5000 \
